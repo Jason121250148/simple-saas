e4b4c6fa24585db6d92a5595a0a7bdcac8177a39
Jason121250148 <962215093@qq.com>
2017-01-10 14:59:23 +0800

    Add process.env.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+const COMPANY_NAME = process.env.COMPANY_NAME;
+
 app.get("/", (req, res) => {
-    console.log("Server is running...");
-    res.send(`Hi docker!`);
+    res.send(`Hi, ${COMPANY_NAME ? COMPANY_NAME : "Stranger"}!`);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running...");
+});
