ca4169aaa5307a0ed6d96752a93335b81610ce31
Jason121250148 <962215093@qq.com>
2017-01-10 17:43:36 +0800

    Add html to response.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
-const COMPANY_NAME = process.env.COMPANY_NAME;
+const COMPANY_NAME = process.env.COMPANY_NAME ? process.env.COMPANY_NAME : "Casiny";
+const MONGODB_HOST = process.env.MONGODB_PORT_27017_TCP_ADDR ? process.env.MONGODB_PORT_27017_TCP_ADDR : "localhost";
+const MONGODB_PORT = process.env.MONGODB_PORT_27017_TCP_PORT ? process.env.MONGODB_PORT_27017_TCP_PORT : 27017;
 
 app.get("/", (req, res) => {
-    res.send(`Hi, ${COMPANY_NAME ? COMPANY_NAME : "Stranger"}!`);
+    res.type("text/html");
+    res.send(`
+        <h1>${COMPANY_NAME}</h1>
+        <ul>
+            <li>Host: ${MONGODB_HOST}</li>
+            <li>Host: ${MONGODB_PORT}</li>
+        </ul>
+    `);
 });
 
 app.listen(8080, () => {
